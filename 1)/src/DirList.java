6a47b33d6a06b7017c3ebe7df1d379324b2e738c
gromov-nikita <hromay.n999@gmail.com>
2021-03-21 15:04:38 +0300

    1

--- 1)/src/DirList.java
+++ 1)/src/DirList.java
@@ -1,17 +1,38 @@
-import java.util.regex.*;
 import java.util.*;
 import java.io.*;
 public class DirList {
+    public static void filesFind(String[] args, File file, List<String> list) {
+        File[] files = file.listFiles();
+        if(args.length == 0) {
+           for(File f : files) {
+               list.add(f.getName());
+               if(f.isDirectory()) {
+                   filesFind(args, f, list);
+               }
+           }
+        }
+        else {
+            //Я тут просто писал регулярное выражение нужное "^\\D+.java"
+            DirFilter dir = new DirFilter(args[0]);
+            for(File f : files) {
+                if(dir.accept(file,f.getName())) {
+                    list.add(f.getName());
+                }
+                if(f.isDirectory()) {
+                    filesFind(args, f, list);
+                }
+            }
+        }
+    }
     public static void main(String[] args) {
         File path = new File(".");
-        String[] list;
-        if(args.length == 0)
-            list = path.list();
-        else
-            list = path.list(new DirFilter(args[0]));
-        Arrays.sort(list, String.CASE_INSENSITIVE_ORDER);
-        for(String dirItem : list)
-            System.out.println(dirItem);
+        List<String> list = new LinkedList<String>();
+        filesFind(args,path,list);
+        //Для проверки сортировки я создал папки  ".idea" (z) и в "1)" (1 a aa ab)
+        Collections.sort(list, String.CASE_INSENSITIVE_ORDER);
+        for(String s : list) {
+            System.out.println(s);
+        }
     }
 }
 
